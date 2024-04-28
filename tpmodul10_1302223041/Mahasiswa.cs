295c1c174051ba529a3e61d5dab32ff0cb745314
Syahreza Adnan Al Azhar <notsupersyahreza@student.telkomuniversity.ac.id>
2024-04-28 22:18:40 +0700

    menambahkan class Mahasiswa

---
+++ tpmodul10_1302223041/Mahasiswa.cs
@@ -0,0 +1,11 @@
+namespace tpmodul10_1302223041
+{
+    public class Mahasiswa
+    {
+        private string Nama { get; set; }
+        private string NIM { get; set; }
+        public Mahasiswa()
+        {
+        }
+    }
+}
